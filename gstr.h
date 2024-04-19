3ff5367506c723dfbee0e5c0cb82099044a09d6c
Dinesh Kumar <gdineshda@gmail.com>
2024-04-19 20:03:48 +0530

    Add append, pop, clear, insert, remove

--- gstr.h
+++ gstr.h
@@ -5,7 +5,21 @@
 #define GSTRING_INITIAL_CAPACITY 16
 #endif //!GSTRING_INITIAL_CAPACITY
 
+#ifndef GSTRING_MINIMUM_CAPACITY
+#define GSTRING_MINIMUM_CAPACITY 8
+#endif // !GSTRING_MINIMUM_CAPACITY
+
 #include <stddef.h>
+
+typedef enum {
+	SOME,
+	NONE
+} option_tag;
+
+typedef struct {
+	option_tag t;
+	char c;
+} opt_char;
 
 typedef struct {
 	char *content;
@@ -20,18 +34,23 @@ typedef struct {
 
 
 gstr gstr_new(void);
+void gstr_clear(gstr *s);
 void gstr_del(gstr *s);
 int gstr_append(gstr *s, char c);
+opt_char gstr_pop(gstr *s);
+int gstr_remove(gstr *s, size_t idx);
 gstr gstr_from_lit(const char *array, size_t len);
 
 #define gstr_from_static(cstr) gstr_from_lit(cstr, sizeof(cstr) - 1)
 
+#define GSTRING_IMPL // For my syntax highlighting
 #ifdef GSTRING_IMPL
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
 
 gstr gstr_new(void) {
-	char *content = malloc(sizeof(char) * GSTRING_INITIAL_CAPACITY);
+	char *content = (char *) malloc(sizeof(char) * GSTRING_INITIAL_CAPACITY);
 	if (content == NULL) return (gstr) {0};
 	gstr str = {
 		.content = content,
@@ -54,7 +73,7 @@ gstr gstr_from_lit(const char *array, size_t len) {
 		.cap = 0,
 	};
 
-	str.content = malloc(sizeof(char) * len);
+	str.content = (char *) malloc(sizeof(char) * len);
 	if (str.content == NULL) {
 		return str;
 	}
@@ -69,10 +88,39 @@ int gstr_grow(gstr *s) {
 	if (res == NULL) {
 		return -1;
 	} else {
-		s -> content = res;
+		s -> content = (char *) res;
 		s -> cap *= 2;
 		return 0;
 	}
+}
+
+int gstr_shrink(gstr *s) {
+	size_t new_size = (size_t) (sizeof(char) * s -> cap * 0.5);
+	void *res = realloc(s -> content, new_size);
+	if (res == NULL) {
+		return -1;
+	} else {
+		s -> content = (char *) res;
+		// BECAREFUL, you are threading on dangerous grounds!
+		s -> cap = new_size;
+		return 0;
+	}
+}
+
+int gstr_insert(gstr *s, size_t idx, char c) {
+	if (idx > s -> len) {
+		return -1;
+	} else if (idx == s -> len) {
+		return gstr_append(s, c);
+	}
+
+	if (s -> len == s -> cap) {
+		if (gstr_grow(s) < 0) return -1;
+	}
+	memmove(s -> content + idx + 1, s -> content + idx, s -> len - idx);
+	s -> content[idx] = c;
+	s -> len += 1;
+	return 0;
 }
 
 int gstr_append(gstr *s, char c) {
@@ -81,6 +129,56 @@ int gstr_append(gstr *s, char c) {
 	}
 	s -> content[s -> len++] = c;
 	return 0;
+}
+
+bool gstr_should_shrink(const size_t len, const size_t cap) {
+	return (len <= (size_t) (cap / 2)) && (cap / 2 > GSTRING_MINIMUM_CAPACITY);
+}
+
+int gstr_remove(gstr *s, size_t idx) {
+	if (idx >= s -> len) {
+		return -1;
+	}
+	memmove(s -> content + idx, s -> content + idx + 1, s -> len - idx);
+
+	s -> len -= 1;
+	if (gstr_should_shrink(s -> len, s -> cap)) {
+		if (gstr_shrink(s) < 0) {
+			return -1;
+		}
+	}
+	return 0;
+}
+
+[[nodiscard]] opt_char gstr_pop(gstr *s) {
+	if (s -> len == 0) {
+		return (opt_char) {
+			.t = NONE,
+			.c = 0,
+		};
+	}
+	char popped = s -> content[s -> len - 1];
+	s -> len -= 1;
+
+	// Only shrink when you need to
+	if (gstr_should_shrink(s -> len, s -> cap)) {
+		int res = gstr_shrink(s);
+		if (res < 0) return (opt_char) {
+			.t = NONE,
+			.c = 0,
+		};
+	}
+
+	return (opt_char) {
+		.t = SOME,
+		.c = popped,
+	};
+}
+
+void gstr_clear(gstr *s) {
+	// Why not?
+	memset(s -> content, '\0', s -> len);
+	s -> len = 0;
 }
 
 #endif // !GSTRING_IMPL
--- test.c
+++ test.c
@@ -37,13 +37,94 @@ int test_append(void) {
 	assert(s.cap == GSTRING_INITIAL_CAPACITY * 2);
 	assert(s.len == 17);
 	gstr_del(&s);
+	return 1;
+}
 
+int test_pop(void) {
+	gstr s = gstr_new();
+	for (int i = 0; i < GSTRING_INITIAL_CAPACITY; i++) {
+		gstr_append(&s, 'x');
+	}
+	printf("%.*s\n", (int) s.len, s.content);
+	for (int i = 0; i < GSTRING_INITIAL_CAPACITY; i++) {
+		opt_char res = gstr_pop(&s);
+		assert(res.t == SOME);
+	}
+	opt_char res = gstr_pop(&s);
+	assert(res.t == NONE);
+
+	gstr name = gstr_from_static("dinesh kumar");
+	res = gstr_pop(&name);
+	assert(res.t == SOME);
+	assert(res.c == 'r');
+
+	gstr_del(&name);
+	gstr_del(&s);
+	return 1;
+}
+
+int test_remove() {
+	gstr s = gstr_new();
+	for (size_t i = 0; i < GSTRING_INITIAL_CAPACITY; i++) {
+		gstr_append(&s, 'a' + i);
+	}
+	printf("%.*s\n", (int) s.len, s.content);
+	gstr_remove(&s, 3);
+	printf("%.*s\n", (int) s.len, s.content);
+	gstr_remove(&s, 0);
+	printf("%.*s\n", (int) s.len, s.content);
+	gstr_remove(&s, s.len - 1);
+	printf("%.*s\n", (int) s.len, s.content);
+	gstr_del(&s);
+	return 1;
+}
+
+int test_insert(void) {
+
+	gstr s = gstr_new();
+
+	for (int i = 0; i < GSTRING_INITIAL_CAPACITY; i++) {
+		gstr_append(&s, 'a' + i);
+	}
+
+	printf("%.*s\n", (int) s.len, s.content);
+	gstr_insert(&s, 0, 'x');
+	assert(s.len == 17);
+	assert(s.cap == 32);
+	assert(s.content[0] == 'x');
+	printf("%.*s\n", (int) s.len, s.content);
+
+	gstr_insert(&s, s.len, 'x');
+	assert(s.len == 18);
+	assert(s.content[s.len - 1] == 'x');
+	printf("%.*s\n", (int) s.len, s.content);
+
+	gstr_del(&s);
+	return 1;
+}
+
+int test_clear(void) {
+	gstr s = gstr_new();
+	for (size_t i = 0; i < s.cap; i++) {
+		gstr_append(&s, 'a' + i);
+	}
+
+	gstr_clear(&s);
+	assert(s.cap == GSTRING_INITIAL_CAPACITY);
+	assert(s.len == 0);
+	assert(s.content[0] == '\0');
+
+	gstr_del(&s);
 	return 1;
 }
 
 int main(void) {
 	test_constructor();
 	test_static_constructor();
-	test_append();
+	test_insert();
+	test_clear();
+	// test_append();
+	// test_pop();
+	// test_remove();
 	return 0;
 }
